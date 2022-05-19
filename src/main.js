231f3932a69fb0d917371a9cb05252c9d45846a6
NaclGolden <541625274@qq.com>
2022-05-19 17:43:23 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
+// 导入字体图标
+import '@/assets/fonts/iconfont.css'
+// 导入element-ui组件及css'
+import '@/plugins/element.js'
+// 导入全局样式表
+import '@/assets/css/global.css'
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://api.dkstudy.com:8000/api/private/v1/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
